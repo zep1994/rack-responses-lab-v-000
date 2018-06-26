2cf6a6366e7eca0e0fcf82031531756771060a6b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-26 17:30:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   
   def call(env)
     resp = Rack::Response.new
-    if Time.now.hour <= 12
+    if Time.now.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
